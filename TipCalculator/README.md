060f6f4651a29130e6b7df29cea397f04a459e54
juhipark <juhipark@student.hartnell.edu>
2019-09-04 21:40:42 -0700

    Additional features implemented

--- TipCalculator/README.md
+++ TipCalculator/README.md
@@ -14,22 +14,22 @@ The following **required** functionality is complete:
 
 The following **optional** features are implemented:
 * [ ] Settings page to change the default tip percentage.
-* [ ] UI animations
-* [ ] Remembering the bill amount across app restarts 
-* [ ] Using locale-specific currency and currency thousands separators.
-* [ ] Making sure the keyboard is always visible and the bill amount is always the first responder. This way the user doesn't have to tap anywhere to use this app. Just launch the app and start typing.
+* [x] UI animations
+* [x] Remembering the bill amount across app restarts 
+* [x] Using locale-specific currency and currency thousands separators.
+* [x] Making sure the keyboard is always visible and the bill amount is always the first responder. This way the user doesn't have to tap anywhere to use this app. Just launch the app and start typing.
 
 The following **additional** features are implemented:
 
 - [ ] List anything else that you can get done to improve the app functionality!
 
--[x]Party size option to divide tip between multiple people and give amount per person
+- [ ] Party size option to divide tip between multiple people and give amount per person
 
 ## Video Walkthrough
 
 Here's a walkthrough of implemented user stories:
 
-<img src='x' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+<img src='TipCalculator.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
 
 GIF created with [LiceCap](http://www.cockos.com/licecap/).
 
--- TipCalculator/TipCalculator/ViewController.swift
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var inputField: UITextField!
     @IBOutlet weak var percentControl: UISegmentedControl!
     @IBOutlet weak var tipLabel: UILabel!
@@ -17,16 +17,51 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // bill amount is always the first responder
+        inputField.becomeFirstResponder()
+        
+        // remebering the bill amount when app restarts
+        inputField.text = UserDefaults.standard.string(forKey: "billAmount") ?? ""
+        
     }
-
+    
+    
     @IBAction func calculateTip(_ sender: Any) {
         
+        if let percentControl = sender as? UISegmentedControl {
+            print("Segmented")
+            
+            percentControl.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+            
+            print("clicked")
+            
+            UIView.animate(withDuration: 2.0, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 6.0, options: .allowUserInteraction, animations:{ [weak self] in
+                self?.percentControl.transform = .identity
+                }, completion: nil)
+        }
+        
         let tipPercent = [0.10, 0.15]
+        
         let bill = Double(inputField.text!) ?? 0
+        
+        // remembering the bill amount
+        UserDefaults.standard.set(inputField.text, forKey: "billAmount")
+        
         let tip = (bill * tipPercent[percentControl.selectedSegmentIndex])
         let total = tip + bill
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+        
+        let currencyFormatter = NumberFormatter()
+        currencyFormatter.usesGroupingSeparator = true
+        currencyFormatter.numberStyle = .currency
+        // localize the way to grouping and decimal separator
+        currencyFormatter.locale = Locale.current
+ 
+        let localeTipString = currencyFormatter.string(from: NSNumber(value:tip))
+        let localeTotalString = currencyFormatter.string(from: NSNumber(value: total))
+        
+        tipLabel.text = localeTipString
+        totalLabel.text = localeTotalString
         
     }
     
