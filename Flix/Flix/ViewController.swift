a0b95a210725a58e4bcb138b3b9e8a442f134eb4
juhipark <juhipark@student.hartnell.edu>
2019-09-16 17:42:53 -0700

    Flix project added

---
+++ Flix/Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Juhi on 9/16/19.
+//  Copyright © 2019 Juhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
