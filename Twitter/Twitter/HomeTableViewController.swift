25b6683cd1d8f6f6ab47c48e690e4e0ddde0c356
juhipark <juhipark@student.hartnell.edu>
2019-10-08 15:37:05 -0700

    Twitter part2 updated

--- Twitter/Twitter/HomeTableViewController.swift
+++ Twitter/Twitter/HomeTableViewController.swift
@@ -19,12 +19,20 @@ class HomeTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        loadTweet()
+//        loadTweet() --> moved to viewDidAppear 
         
         myRefreshControl.addTarget(self, action: #selector(loadTweet), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
 
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        loadTweet()
+    }
+    
     
     @objc func loadTweet(){
         
@@ -64,12 +72,16 @@ class HomeTableViewController: UITableViewController {
         cell.userNameLabel.text = user["name"] as? String
         cell.tweetContent.text = tweetArray[indexPath.row]["text"] as? String
         
-       let imageUrl = URL(string:   (user["profile_image_url_https"] as? String)!)
+        let imageUrl = URL(string:   (user["profile_image_url_https"] as? String)!)
         let data = try? Data(contentsOf: imageUrl!)
         
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         return cell
     }
